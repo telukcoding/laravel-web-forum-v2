b863b73e3dda2b22c9e7811c3e44644720361e1f
telukcoding <telukcoding@gmail.com>
2018-05-27 19:18:03 +0800

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,32 @@
+<div class="container"> 
+  <div class="jumbotron" id="tc_jumbotron_footer">
+    <div class="spane"></div> 
+        <div class="card-body">  
+        <div class="row">
+        <div class="col-md-6"><br>
+           <div class="tc_head_title_footer1"> 
+            <h2>Teluk Coding</h2>  
+             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five</p>           
+           </div>
+        </div>
+        <div class="col-md-6"><br>
+            <div class="tc_head_title_footer1">   
+              <div class="footer_about">
+                <a href="">About</a>
+                <a href="">Profile</a>
+                <a href="">Blog</a>
+                <a href="">Contact</a>
+                <a href="">Help</a>
+              </div> <hr style="margin-bottom: 5px">
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+            <div class="footer_sosial">
+               <a href=""><i class="fa fa-facebook"></i></a>
+               <a href=""><i class="fa fa-twitter"></i></a>
+               <a href=""><i class="fa fa-google-plus"></i></a>
+            </div>
+           </div>
+        </div> 
+      </div> 
+    </div> 
+  </div>   
+</div>
